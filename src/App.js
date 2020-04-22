30d0508f7248cc37064565ee3edfd9915dc88c88
Suzy <suzy.nakayama@gmail.com>
2020-04-21 23:07:28 -0400

    added myfooter to test

--- src/App.js
+++ src/App.js
@@ -11,23 +11,25 @@ import CookieContainer from "./components/CookieContainer";
 import HooksCookieContainer from "./components/HooksCookieContainer";
 import NewIceCreamContainer from "./components/NewIceCreamContainer";
 import NewCookieContainer from "./components/NewCookieContainer";
+import Footer from "react-myfooter";
 
 function App() {
-    return (
-        <Provider store={store}>
-            <div className="App">
-                <CakeContainer />
-                <HooksCakeContainer />
-                <IceCreamContainer />
-                <HooksIceCreamContainer />
-                <CookieContainer />
-                <HooksCookieContainer />
-                <NewCakeContainer />
-                <NewIceCreamContainer />
-                <NewCookieContainer />
-            </div>
-        </Provider>
-    );
+	return (
+		<Provider store={store}>
+			<div className="App">
+				<CakeContainer />
+				<HooksCakeContainer />
+				<IceCreamContainer />
+				<HooksIceCreamContainer />
+				<CookieContainer />
+				<HooksCookieContainer />
+				<NewCakeContainer />
+				<NewIceCreamContainer />
+				<NewCookieContainer />
+				<Footer github="https://www.suzynakayama.com" name="Suzy Nakayama" />
+			</div>
+		</Provider>
+	);
 }
 
 export default App;
